c774ba062b338512b4a58ebc020c267d66ff050a
Eren Gazioglu <pompyproduction@gmail.com>
2023-11-06 22:53:26 +0100

    Set up basic sass structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import reactDom from "react-dom";
 import App from "./react/App";
+import "./sass/style.scss";
 
 reactDom.render(<App />, document.getElementById("root"));
