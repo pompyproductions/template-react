e6e664eb150f0d26bcca3874c96e5d6514fc042e
Eren Gazioglu <pompyproduction@gmail.com>
2023-11-07 13:51:16 +0100

    Create basic page layout

--- src/react/App.js
+++ src/react/App.js
@@ -11,6 +11,7 @@ const App = () => {
     <React.StrictMode>
       <BrowserRouter>
         <Header />
+        <div className="vsep"></div>
         <Routes>
           <Route path="*" element={<Home />}/>
           <Route path="/page" element={<OtherPage />}/>
